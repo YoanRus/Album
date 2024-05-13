5b056218c08769c42319caea750c50a3f6e5097c
YoanRus <tabyle@outlook.com>
2024-05-14 00:14:46 +0300

    Add files via upload

---
+++ Album Project 2024/src/FrameCustom.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class FrameCustom extends Frame{
+   FrameCustom() {
+       JFrame frame = new JFrame("Album Organized");
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setSize(720, 480);
+       JButton Button1 = new JButton("Add Image");
+       JButton Button2 = new JButton("Catagories");
+       JButton Button3 = new JButton("Upload File");
+       JPanel panel = new JPanel();
+
+       panel.add(Button1);
+       //panel.add(Box.createRigidArea(new Dimension(5, 0)));
+       //We can add spaces if we prefer to do so
+
+       panel.add(Button2);
+       // panel.add(Box.createRigidArea(new Dimension(5, 0)));
+
+       panel.add(Button3);
+       Button1.addActionListener(new ActionListener() {
+           public void actionPerformed(ActionEvent e) {
+               JDialog dialog = new JDialog();
+               dialog.setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
+               dialog.setSize(400, 400);
+                Button ButtonSelectFile = new Button("Select File");
+                ButtonSelectFile.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        JFileChooser fileChooser = new JFileChooser();
+                        fileChooser.showOpenDialog(null);
+                        String filepath = fileChooser.getSelectedFile().getAbsolutePath();
+                        System.out.println(filepath);
+                    }
+
+                });
+                dialog.add(ButtonSelectFile);
+               dialog.setVisible(true);
+           }
+       });
+       panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+       frame.add(panel);
+       Image image = Toolkit.getDefaultToolkit().getImage("Icon.png");
+       frame.setIconImage(image);
+       frame.setVisible(true);
+   }
+}
--- Album Project 2024/src/Main.java
+++ Album Project 2024/src/Main.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
@@ -6,17 +8,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Album Organized");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1200, 1200);
-        JButton Button1 = new JButton("VERTICAL1");
-        JButton Button2 = new JButton("VERTICAL2");
-        JButton Button3 = new JButton("VERTICAL3");
-        frame.add(Button1, BorderLayout.NORTH);
-        frame.add(Button3,BorderLayout.NORTH);
-        frame.add(Button2);
-        Image image = Toolkit.getDefaultToolkit().getImage("Icon.png");
-        frame.setIconImage(image);
-        frame.setVisible(true);
+        FrameCustom frame = new FrameCustom();
     }
 }
